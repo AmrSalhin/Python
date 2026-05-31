76ee15f827fedef6a3935ee6d45ace57b13cb625
Amr Salhin <amr.a.salhin@gmail.com>
2026-05-31 21:23:53 +0300

    generate notebook in pdf format

---
+++ pdf-generator/main.py
@@ -0,0 +1,35 @@
+from fpdf import FPDF
+import pandas as pd
+
+#extract data frame from csv file
+df = pd.read_csv("topics.csv")
+
+# p --> portrait    mm --> milli meter
+pdf = FPDF(orientation='P', unit='mm', format = 'A4')
+pdf.set_auto_page_break(auto=False ,margin=0)
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font('Times', 'B', size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
+    for y in range(20, 298, 10):
+        pdf.line(10,y,200,y)
+
+    #set footer
+    pdf.ln(265)
+    pdf.set_font('Times', 'I', size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row['Topic'], align='R', ln=1)
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+        # set footer
+        pdf.ln(277)
+        pdf.set_font('Times', 'I', size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row['Topic'], align='R', ln=1)
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
+
+pdf.output("OUTPUT.pdf")
