a884593c5f1319ccf00e743674ff0320108dedf1
Amr Salhin <amr.a.salhin@gmail.com>
2026-06-10 22:58:40 +0300

    Call Api and send its content via email

---
+++ EmailNewsApp/main.py
@@ -0,0 +1,20 @@
+import requests
+import send_email
+
+
+apiKey = "9d86746948ca4c5389d4d59f0e19c7ea"
+url = ("https://newsapi.org/v2/everything?q=tesla&"
+       "from=2026-05-10&sortBy=publishedAt&"
+       "apiKey=9d86746948ca4c5389d4d59f0e19c7ea")
+
+request = requests.get(url)
+
+content = request.json()
+
+body = ""
+for article in content["articles"]:
+    body += str(article["title"]) + "\n"
+    body += str(article["description"]) + 2*"\n"
+
+body = body.encode("utf-8")
+send_email.send_email(message=body)
